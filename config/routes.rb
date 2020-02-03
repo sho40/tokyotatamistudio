ab3f6ae215374f959c4c1916191c45ae239da952
sho40 <kanazawa.sho40@gmail.com>
2020-02-04 01:35:52 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,14 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'products#index'
+
+  resources :products
+
+  resources :carts, only: [:show]
+
+  post '/add_item' => 'carts#add_item'
+  post '/update_item' => 'carts#update_item'
+  delete '/delete_item' => 'carts#delete_item'
+
+
 end
