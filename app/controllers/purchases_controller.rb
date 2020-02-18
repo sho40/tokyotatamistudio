7ecfc0d18f40766f75bbfae41e23dd3abfb3d87d
sho40 <kanazawa.sho40@gmail.com>
2020-02-18 20:16:12 +0900

    Update purchases_controller.rb

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,6 +1,15 @@
 class PurchasesController < ApplicationController
   before_action :authenticate_customer!
+  before_action :setup_cart_item!
+
   def new
+    @cart_items = current_cart.cart_items
+  end
+
+  private
+
+  def setup_cart_item!
+    @cart_item = current_cart.cart_items.find_by(product_id: params[:product_id])
   end
 
 
