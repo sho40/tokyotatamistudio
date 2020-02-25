c0c5a7a7a027743ac987c346423c818aec77d5c9
sho40 <kanazawa.sho40@gmail.com>
2020-02-25 20:43:10 +0900

    pay.jp

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -6,6 +6,15 @@ class PurchasesController < ApplicationController
     @cart_items = current_cart.cart_items
   end
 
+  def pay
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: params[:amount],
+      card: params['payjp-token'],
+      currency: 'jpy'
+    )
+  end
+
 
   private
 
