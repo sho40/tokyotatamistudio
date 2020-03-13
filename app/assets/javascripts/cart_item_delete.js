4eed12837911d4f88fc6481992110daa230d7529
sho40 <kanazawa.sho40@gmail.com>
2020-03-13 17:27:43 +0900

    cart-content

--- app/assets/javascripts/cart_item_delete.js
+++ app/assets/javascripts/cart_item_delete.js
@@ -1,11 +1,11 @@
 $(document).on('turbolinks:load' , function(){
   $(function(){
-    $('.delete_btn').on('click', function(){
+    $('.cart-content__main__current-cart__box__middle__product__sum__delete_btn').on('click', function(){
       var deleteConfirm = confirm('カートから削除しますか？');
 
       if(deleteConfirm == true) {
         var clickEle = $(this)
-        var itemID = clickEle.parent().attr("item-id");
+        var itemID = clickEle.parent().parent().parent().attr("item-id");
 
         $.ajax({
           url: '/delete_item',
@@ -15,7 +15,7 @@ $(document).on('turbolinks:load' , function(){
         })
 
         .done(function(){
-          clickEle.parent().remove();
+          clickEle.parent().parent().parent().remove();
         })
 
         .fail(function(){
