99dcbf585142c4a07040753ddb1e96f61b51dde5
sho40 <kanazawa.sho40@gmail.com>
2020-03-28 19:34:50 +0900

    Update Payjp.js

--- app/assets/javascripts/Payjp.js
+++ app/assets/javascripts/Payjp.js
@@ -1,37 +1,35 @@
 $(document).on('turbolinks:load',function(){
 
-  var form = $("#charge-form")
-      number = form.find('input[name="number"]'),
-      exp_year = form.find('input[name="exp_year"]'),
-      exp_month = form.find('input[name="exp_month"]'),
-      cvc = form.find('input[name="cvc"]');
-  $("#open-modal").click(function() {
+  var form = $(".card-form")
+  $("#charge-form").click(function() {
     form.find("button[type=submit]").prop("disabled", true);
 
     Payjp.setPublicKey('pk_test_249441c3c84fde3e191db39c');
 
     var card = {
-      number: number.val(),
-      cvc: cvc.val(),
-      exp_month: exp_month.val(),
-      exp_year: exp_year.val()
+      number: $("#card_number").val(),
+      cvc: $("#cvc").val(),
+      exp_month: $("#exp_month").val(),
+      exp_year: $("#exp_year").val(),
+      name: $('#name').val()
     };
 
 
-    Payjp.createToken(card, function(s, response) {
+    Payjp.createToken(card, function(status, response) {
       if (response.error) {
         form.find('.payment-errors').text(response.error.message);
         form.find('button[type=submit]').prop('disabled', false);
       }
       else {
-        $(".card-modalWindow__content__form__number__input").removeAttr("name");
-        $(".card-modalWindow__content__form__sub__cvc__input").removeAttr("name");
-        $(".card-modalWindow__content__form__sub__wide-field__month__input").removeAttr("name");
-        $(".card-modalWindow__content__form__sub__wide-field__year__input").removeAttr("name");
+        $("#card_number").removeAttr("name");
+        $("#cvc").removeAttr("name");
+        $("#exp_month").removeAttr("name");
+        $("#exp_year").removeAttr("name");
+        $("#name").removeAttr("name")
 
         var token = response.id;
 
-        form.append($('<input type="hidden" name="payjp-token" />').val(token));
+        form.append($('<input type="hidden" name="payjpToken" />').val(token));
         form.get(0).submit();
       }
     });
