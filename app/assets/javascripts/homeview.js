7e0cfdce2133bb01912aaea22b231b440800b132
sho40 <kanazawa.sho40@gmail.com>
2020-04-04 18:16:31 +0900

    Create homeview.js

---
+++ app/assets/javascripts/homeview.js
@@ -0,0 +1,12 @@
+$(function(){
+  
+  $(".home-content , .footer").css("display", "none");
+  setTimeout(function(){
+    // $(".mainvisual").css("border-width", "7rem 8vw");
+    $(".home-content , .footer").css("display", "block");
+
+  }, 1800)
+
+  $(".mainvisual").animate
+
+})
