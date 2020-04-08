75ba18d399007a3271689f075182d72118157e91
sho40 <kanazawa.sho40@gmail.com>
2020-04-08 21:07:42 +0900

    form-label-animation

---
+++ app/assets/javascripts/contact-form.js
@@ -0,0 +1,15 @@
+$(document).on('turbolinks:load',function(){
+  $(function(){
+
+    $('input').on('focusin', function(){
+      $(this).parent().find('label').addClass('active');
+    });
+
+    $('input').on('focusout', function(){
+      if (!this.value) {
+        $(this).parent().find('label').removeClass('active');
+      }
+    });
+
+  });
+});
