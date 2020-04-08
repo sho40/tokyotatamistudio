eec3ed2b9e164f60173f9d3ef3b5be82723d631f
sho40 <kanazawa.sho40@gmail.com>
2020-04-09 01:31:58 +0900

    change function-class

--- app/assets/javascripts/contact-form.js
+++ app/assets/javascripts/contact-form.js
@@ -1,11 +1,11 @@
 $(document).on('turbolinks:load',function(){
   $(function(){
 
-    $('input').on('focusin', function(){
+    $('.nice-textbox').on('focusin', function(){
       $(this).parent().find('label').addClass('active');
     });
 
-    $('input').on('focusout', function(){
+    $('.nice-textbox').on('focusout', function(){
       if (!this.value) {
         $(this).parent().find('label').removeClass('active');
       }
