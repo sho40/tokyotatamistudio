efe2ea2ae89b62889627b2a810fbde088536a5af
sho40 <kanazawa.sho40@gmail.com>
2020-04-11 01:37:12 +0900

    Update session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,2 +1,2 @@
 app_name = "tokyotatamistudio"
-Rails.application.config.session_store :cookie_store, key: "_#{app_name}_session", expire_after: 1.day
+Rails.application.config.session_store :cookie_store, key: "_#{app_name}_session", expire_after: 2.hour
