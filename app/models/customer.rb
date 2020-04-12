c956cdb1d65708a1f333911eb4a7c0f33f70c112
sho40 <kanazawa.sho40@gmail.com>
2020-04-12 22:34:54 +0900

    model change

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,6 +6,10 @@ class Customer < ApplicationRecord
 
   include JpPrefecture
   jp_prefecture :prefecture_code
+
+  validates :name, presence: true
+  VALID_PHONE_REGEX = /\A(((0(\d{1}[-(]?\d{4}|\d{2}[-(]?\d{3}|\d{3}[-(]?\d{2}|\d{4}[-(]?\d{1}|[5789]0[-(]?\d{4})[-)]?)|\d{1,4}\-?)\d{4}|0120[-(]?\d{3}[-)]?\d{3})\z/
+  validates :tel, presence: true, format: { with: VALID_PHONE_REGEX }
   
   def prefecture_name
     JpPrefecture::Prefecture.find(code: prefecture_code).try(:name)
