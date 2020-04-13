9afc772c8749f80cab418a62d9900eaf2971af09
sho40 <kanazawa.sho40@gmail.com>
2020-04-13 16:38:09 +0900

    wright_validation

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -10,6 +10,17 @@ class Customer < ApplicationRecord
   validates :name, presence: true
   VALID_PHONE_REGEX = /\A(((0(\d{1}[-(]?\d{4}|\d{2}[-(]?\d{3}|\d{3}[-(]?\d{2}|\d{4}[-(]?\d{1}|[5789]0[-(]?\d{4})[-)]?)|\d{1,4}\-?)\d{4}|0120[-(]?\d{3}[-)]?\d{3})\z/
   validates :tel, presence: true, format: { with: VALID_PHONE_REGEX }
+
+  before_save { self.email = email.downcase }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, {presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }}
+
+  validates :postcode, presence: true
+  validates :prefecture_code, presence: true
+  validates :address_city, presence: true
+  validates :address_street, presence: true
+
+  validates :password_confirmation, presence: true
   
   def prefecture_name
     JpPrefecture::Prefecture.find(code: prefecture_code).try(:name)
