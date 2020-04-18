63e916e6e155ad864c9294fcc596b4ade5c37b23
sho40 <kanazawa.sho40@gmail.com>
2020-04-19 00:51:28 +0900

    S3

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,8 +4,13 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
   # storage :fog
+
+  if Rails.env.development? || Rails.env.test?
+    storage :file
+  else
+    storage :fog
+  end
 
   process resize_to_fit: [200, 200]
 
