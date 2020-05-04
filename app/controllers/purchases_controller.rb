070cfbbb6c2199e7509794d398afffcfb918c684
sho40 <kanazawa.sho40@gmail.com>
2020-05-04 21:32:37 +0900

    fraight

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -8,30 +8,53 @@ class PurchasesController < ApplicationController
     @cart_items.each do |item|
       @subtotal = item.product.price * item.quantity
       @total << @subtotal
+      if @total.sum >= 7000
+        @freight = 0
+      else
+        @freight = 200
+      end
     end
   end
 
   def pay
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-    Payjp::Charge.create(
-      amount: params[:amount],
-      card: params['payjpToken'],
-      currency: 'jpy',
-    )
     purchase = Purchase.new(purchase_params)
     @cart_items = current_cart.cart_items
     @customer = Customer.find_by(id: purchase.customer_id)
-    if purchase.save
-      @cart_items.each do |item|
-        @stock = item.product.stock -= item.quantity
-        item.product.update(stock: @stock)
+    if params[:cache] #現金決済
+      if purchase.save
+        @cart_items.each do |item|
+          @stock = item.product.stock -= item.quantity
+          item.product.update(stock: @stock)
+        end
+        session[:cart_id] = nil
+        NotificationMailer.send_cache_confirm_to_customer(@customer).deliver
+        redirect_to purchases_cache_path
+      else
+        render :index
       end
-      session[:cart_id] = nil
-      NotificationMailer.send_confirm_to_customer(@customer).deliver
     else
-      render :index
+      Payjp.api_key = ENV["PAYJP_SECRET_KEY"] #カード決済
+      Payjp::Charge.create(
+        amount: params[:amount],
+        card: params['payjpToken'],
+        currency: 'jpy',
+      )
+      if purchase.save
+        @cart_items.each do |item|
+          @stock = item.product.stock -= item.quantity
+          item.product.update(stock: @stock)
+        end
+        session[:cart_id] = nil
+        NotificationMailer.send_confirm_to_customer(@customer).deliver
+      else
+        render :index
+      end
     end
   end
+
+  def cache
+  end
+
 
 
   private
