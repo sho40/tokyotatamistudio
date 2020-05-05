591f5139886b055ad6a97aa647c34157cd803ce7
sho40 <kanazawa.sho40@gmail.com>
2020-05-05 14:50:04 +0900

    Update cache_confarm.js

--- app/assets/javascripts/cache_confarm.js
+++ app/assets/javascripts/cache_confarm.js
@@ -1,7 +1,7 @@
 $(document).on('turbolinks:load',function(){
   $(function(){
     $('.confarm-content__main__purchase__purchase-link__bank-transfer').click(function() {
-      if (!confirm('現金振込みで確定致しますか？')) {
+      if (!confirm('振込手数料はお客様負担となります。 現金振込みで確定致しますか？')) {
         return false;
       } else {
         location.href = 'cache.html';
