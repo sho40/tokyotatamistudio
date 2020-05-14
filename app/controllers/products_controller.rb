1e89f6c2f7b3df63ce619d1141a730c9bf5f19d8
sho40 <kanazawa.sho40@gmail.com>
2020-05-15 02:53:37 +0900

    Update products_controller.rb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,7 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all.order(created_at: "DESC")
     @categories = Category.all
-    @frames = Product.where(category_id: 3)
+    @frames = Product.where(category_id: 3).order(created_at: "DESC")
   end
 
   def show
