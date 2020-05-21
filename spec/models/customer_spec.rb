1252d623dd7bf868aa6b70130586d8c5b6ac48d1
sho40 <kanazawa.sho40@gmail.com>
2020-05-21 23:36:07 +0900

    factory_bot

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -5,77 +5,77 @@ RSpec.describe Customer, type: :model do
 
     #name
     it "nameがない場合は登録できないこと" do
-      customer = Customer.new(name: "",tel: "09012345676",email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000", postcode: "1900013", prefecture_code: "東京都", address_city: "立川市", address_street: "一番町345-11")
+      customer = build(:customer, name: "")
       customer.valid?
       expect(customer.errors[:name]).to include("を入力してください")
     end
     
     #tel
     it "telがない場合は登録できないこと" do
-      customer = Customer.new(name: "武",tel: "",email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000", postcode: "1900013", prefecture_code: "東京都", address_city: "立川市", address_street: "一番町345-11")
+      customer = build(:customer, tel: "")      
       customer.valid?
       expect(customer.errors[:tel]).to include("を入力してください")
     end
 
     it "telが11桁以上の場合は登録できない" do
-      customer = Customer.new(name: "武",tel: "09012345676333",email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000", postcode: "1900013", prefecture_code: "東京都", address_city: "立川市", address_street: "一番町345-11")
+      customer = build(:customer, tel: "090123412341")      
       customer.valid?
       expect(customer.errors[:tel]).to include("は不正な値です")
     end
     
     it "telの始まりが000は登録できない" do
-      customer = Customer.new(name: "武",tel: "00045676456",email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000", postcode: "1900013", prefecture_code: "東京都", address_city: "立川市", address_street: "一番町345-11")
+      customer = build(:customer, tel: "00012341234")      
       customer.valid?
       expect(customer.errors[:tel]).to include("は不正な値です")
     end
 
     #email
     it "emailがない場合は登録できないこと" do
-      customer = Customer.new(name: "武",tel: "09012345676",email: "", password: "00000000", password_confirmation: "00000000", postcode: "1900013", prefecture_code: "東京都", address_city: "立川市", address_street: "一番町345-11")
+      customer = build(:customer, email: "")      
       customer.valid?
       expect(customer.errors[:email]).to include("を入力してください")
     end
 
     it "@マークのあとのドメイン" do
-      customer = Customer.new(name: "武",tel: "09012345676",email: "example@uuu", password: "00000000", password_confirmation: "00000000", postcode: "1900013", prefecture_code: "東京都", address_city: "立川市", address_street: "一番町345-11")
+      customer = build(:customer, email: "example@eee")
       customer.valid?
       expect(customer.errors[:email]).to include("は不正な値です")
     end
 
     #password
     it "passwordがない場合は登録できないこと" do
-      customer = Customer.new(name: "武",tel: "09012345676",email: "kkk@gmail.com", password: "", password_confirmation: "00000000", postcode: "1900013", prefecture_code: "東京都", address_city: "立川市", address_street: "一番町345-11")
+      customer = build(:customer, password: "")
       customer.valid?
       expect(customer.errors[:password]).to include("を入力してください")
     end
 
     it "password_confirmationがない場合は登録できないこと" do
-      customer = Customer.new(name: "武",tel: "09012345676",email: "kkk@gmail.com", password: "00000000", password_confirmation: "", postcode: "1900013", prefecture_code: "東京都", address_city: "立川市", address_street: "一番町345-11")
+      customer = build(:customer, password_confirmation: "")
       customer.valid?
       expect(customer.errors[:password_confirmation]).to include("を入力してください")
     end
 
     #住所
     it "postcodeがない場合は登録できないこと" do
-      customer = Customer.new(name: "武",tel: "09012345676",email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000", postcode: "", prefecture_code: "東京都", address_city: "立川市", address_street: "一番町345-11")
+      customer = build(:customer, postcode: "")
       customer.valid?
       expect(customer.errors[:postcode]).to include("を入力してください")
     end
 
     it "prefecture_codeがない場合は登録できないこと" do
-      customer = Customer.new(name: "武",tel: "09012345676",email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000", postcode: "1900013", prefecture_code: "", address_city: "立川市", address_street: "一番町345-11")
+      customer = build(:customer, prefecture_code: "")
       customer.valid?
       expect(customer.errors[:prefecture_code]).to include("を入力してください")
     end
 
     it "address_cityがない場合は登録できないこと" do
-      customer = Customer.new(name: "武",tel: "09012345676",email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000", postcode: "1900013", prefecture_code: "東京都", address_city: "", address_street: "一番町345-11")
+      customer = build(:customer, address_city: "")
       customer.valid?
       expect(customer.errors[:address_city]).to include("を入力してください")
     end
 
     it "address_streetがない場合は登録できないこと" do
-      customer = Customer.new(name: "武",tel: "09012345676",email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000", postcode: "1900013", prefecture_code: "東京都", address_city: "立川市", address_street: "")
+      customer = build(:customer, address_street: "")
       customer.valid?
       expect(customer.errors[:address_street]).to include("を入力してください")
     end
