af6f7c046b0915eb7bc23c9856567601b73e55ab
sho40 <kanazawa.sho40@gmail.com>
2020-06-03 23:49:30 +0900

    Update mail.rb

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,11 +1,15 @@
 if Rails.env.production?
+
+  mail = ENV['OWNER_ADDRESS']
+  pass = ENV['OWNER_ADDRESS_PASSWORD']
+
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     address: 'smtp.gmail.com',
     domain: 'gmail.com',
     port: 587,
-    user_name: ENV['OWNER_ADDRESS'],
-    password: ENV['carfsgpbfwqepile'],
+    user_name: mail,
+    password: pass,
     authentication: 'plain',
     enable_starttls_auto: true
   }
@@ -14,7 +18,3 @@ elsif Rails.env.development?
 else
   ActionMailer::Base.delivery_method = :test
 end
-
-
-
-# OWNER_ADDRESS_PASSWORD
