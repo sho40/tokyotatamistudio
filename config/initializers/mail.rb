9d4eaca047e9d05ce169e484de18c4f34a42a9cd
sho40 <kanazawa.sho40@gmail.com>
2020-06-04 00:50:14 +0900

    Update mail.rb

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,12 +1,12 @@
 if Rails.env.production?
 
-  mail = ENV['OWNER_ADDRESS']
-  pass = ENV['OWNER_ADDRESS_PASSWORD']
+  mail = ENV["OWNER_ADDRESS"]
+  pass = ENV["OWNER_ADDRESS_PASSWORD"]
 
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
-    address: 'smtp.gmail.com',
-    domain: 'gmail.com',
+    address: "smtp.gmail.com",
+    domain: "gmail.com",
     port: 587,
     user_name: mail,
     password: pass,
