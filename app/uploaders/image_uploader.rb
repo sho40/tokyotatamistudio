3ba2f04cba92feb00d45a17e8b030c9455196ba3
sho40 <kanazawa.sho40@gmail.com>
2020-06-07 18:13:56 +0900

    Update image_uploader.rb

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -12,6 +12,8 @@ class ImageUploader < CarrierWave::Uploader::Base
     storage :fog
   end
 
+  # process resize_to_fit: [200, 200]
+
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
