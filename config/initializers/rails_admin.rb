0e50fee3f98dd5ce278da08b951ef597b6566397
sho40 <kanazawa.sho40@gmail.com>
2020-06-08 15:31:51 +0900

    rails_admin_delete

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,53 +1,53 @@
-RailsAdmin.config do |config|
+# RailsAdmin.config do |config|
 
-  ### Popular gems integration
+#   ### Popular gems integration
 
-  ## == Devise ==
-  config.authenticate_with do
-    warden.authenticate! scope: :user
-  end
-  config.current_user_method(&:current_user)
+#   ## == Devise ==
+#   config.authenticate_with do
+#     warden.authenticate! scope: :user
+#   end
+#   config.current_user_method(&:current_user)
 
-  ## == CancanCan ==
-  config.authorize_with :cancancan
+#   ## == CancanCan ==
+#   config.authorize_with :cancancan
 
-  ## == Pundit ==
-  # config.authorize_with :pundit
+#   ## == Pundit ==
+#   # config.authorize_with :pundit
 
-  ## == PaperTrail ==
-  # config.audit_with :paper_trail, 'User', 'PaperTrail::Version' # PaperTrail >= 3.0.0
+#   ## == PaperTrail ==
+#   # config.audit_with :paper_trail, 'User', 'PaperTrail::Version' # PaperTrail >= 3.0.0
 
-  ### More at https://github.com/sferik/rails_admin/wiki/Base-configuration
+#   ### More at https://github.com/sferik/rails_admin/wiki/Base-configuration
 
-  ## == Gravatar integration ==
-  ## To disable Gravatar integration in Navigation Bar set to false
-  # config.show_gravatar = true
+#   ## == Gravatar integration ==
+#   ## To disable Gravatar integration in Navigation Bar set to false
+#   # config.show_gravatar = true
 
-  # config.model Product do
-  #   edit do
-  #     # 全てのフィールドを加える
-  #     include_all_fields
-  #     field :text do
-  #       f.text_area true
-  #     end
-  #   end
-  # end
+#   # config.model Product do
+#   #   edit do
+#   #     # 全てのフィールドを加える
+#   #     include_all_fields
+#   #     field :text do
+#   #       f.text_area true
+#   #     end
+#   #   end
+#   # end
 
 
 
-  config.actions do
-    dashboard                     # mandatory
-    index                         # mandatory
-    new
-    export
-    bulk_delete
-    show
-    edit
-    delete
-    show_in_app
+#   config.actions do
+#     dashboard                     # mandatory
+#     index                         # mandatory
+#     new
+#     export
+#     bulk_delete
+#     show
+#     edit
+#     delete
+#     show_in_app
 
-    ## With an audit adapter, you can add:
-    # history_index
-    # history_show
-  end
-end
+#     ## With an audit adapter, you can add:
+#     # history_index
+#     # history_show
+#   end
+# end
